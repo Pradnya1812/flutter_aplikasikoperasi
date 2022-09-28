963f37b97473b5c36010e923fd95394d7f0e8bae
Pradnya1812 <swariwijaya@gmail.com>
2022-09-28 16:57:18 +0800

    Profil Nasabah

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -11,7 +11,90 @@ class _dasboardState extends State<dasboard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text('Koperasi Undiksha'),
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(255, 8, 5, 130),
+      ),
+      body: SingleChildScrollView(
+        child: SafeArea(
+            child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                padding: EdgeInsets.all(20.0),
+                //height: 100.0,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10.0),
+                    border: Border.all(
+                      color: Color.fromARGB(255, 8, 5, 130),
+                    )),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      child: Image(
+                        image: AssetImage('images/logoundiksha.jpeg'),
+                        width: 200,
+                        height: 200,
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Container(
+                            width: 300,
+                            padding: EdgeInsets.all(20.0),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10.0),
+                                color: Color.fromARGB(255, 183, 199, 255)),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('Nasabah',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold)),
+                                Text('Ni Made Pradnyaswari Wijaya'),
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Container(
+                            width: 300,
+                            padding: EdgeInsets.all(20.0),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10.0),
+                                color: Color.fromARGB(255, 183, 199, 255)),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('Total Saldo Anda',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold)),
+                                Text('Rp. 500.000')
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        )),
+      ),
     );
   }
 }
