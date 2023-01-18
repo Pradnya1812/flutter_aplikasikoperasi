7184ba25fd0677959215a4c77c91723c49b1196b
Pradnya1812 <swariwijaya@gmail.com>
2023-01-18 21:36:56 +0800

    trasfer

--- lib/tampilan_kategori/transfer.dart
+++ lib/tampilan_kategori/transfer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_aplikasikoperasi/model/list_users_model.dart';
 import 'package:flutter_aplikasikoperasi/service/list_users_service.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class Transfer extends StatelessWidget {
   const Transfer({Key? key, required this.usersModel}) : super(key: key);
@@ -37,4 +38,43 @@ class Transfer extends StatelessWidget {
       )),
     );
   }
+}
+
+showNotification() async {
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('@mipmap/ic_launcher');
+
+  const DarwinInitializationSettings initializationSettingsIOS =
+      DarwinInitializationSettings(
+    requestSoundPermission: false,
+    requestBadgePermission: false,
+    requestAlertPermission: false,
+  );
+  const InitializationSettings initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,
+    iOS: initializationSettingsIOS,
+  );
+
+  await flutterLocalNotificationsPlugin.initialize(
+    initializationSettings,
+  );
+
+  AndroidNotificationChannel channel = const AndroidNotificationChannel(
+    'high channel',
+    'Very important notification!!',
+    description: 'the first notification',
+    importance: Importance.max,
+  );
+
+  await flutterLocalNotificationsPlugin.show(
+      1,
+      'Koperasi Undiksha',
+      'Berhasil Transfer',
+      NotificationDetails(
+        android: AndroidNotificationDetails(channel.id, channel.name,
+            channelDescription: channel.description),
+      ));
 }
